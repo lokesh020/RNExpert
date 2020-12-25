3303c645256ede9f0ac30a3a5abaa07e34927136
Lokesh Jangid <lokeshjangid020@gmail.com>
2020-12-25 17:40:16 +0530

    RNVectorIcons added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {View,Text} from 'react-native';
 import {HelloWorld} from '_atoms';
-import {Typography} from '_styles';
+import {Typography,Colors} from '_styles';
 import UserModel from "../src/models/UserModel";
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   const userModel = new UserModel({username:"Lokesh The Developer"})
   return  (
-    <View style = {{justifyContent:"center",alignItems:"center", flex:1}}>
+    <View style = {{justifyContent:"center",alignItems:"center", flex:1,flexDirection:"row"}}>
       <HelloWorld name = {userModel.username} style = {{fontSize: Typography.FONT_SIZE_16}}/>
+      <Icon name="customerservice" size={80} color={Colors.SUCCESS}/>
     </View>
   )
 } 
